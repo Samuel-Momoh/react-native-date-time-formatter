10c4f80012e5ea90446af1df4ef7dff2ef001619
Samuel-Momoh <samueldan@live.com>
2021-12-29 10:28:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import {Formater} from './formatter'
+import {FormateDateTime} from './formatDateTime'
+import { FormatMonthDays,FormatMonth,FormatDay,FormatDate,FormatHours,FormatMinutes,FormatSeconds,FormatTime,FormatYear } from "./helper";
+
+
+export {
+  Formater,
+  FormateDateTime,
+  FormatMonthDays,
+  FormatYear,
+  FormatMonth,
+  FormatDay,
+  FormatDate,
+  FormatHours,
+  FormatMinutes,
+  FormatSeconds,
+  FormatTime
+}
